e74f09ad25b97173479895c091800c730d84c903
fhackenberger <florian@hackenberger.at>
2016-12-03 14:23:46 +0100

    Added click action to map for initial query; Added log notifications

--- app.js
+++ app.js
@@ -3,33 +3,36 @@
 // parameter when you first load the API.
 var useLocalDatasources = true;
 
-var map;
-var infowindow;
-
-// TODO s
-// * Chose the farthest carsharing car as the start location
-// * Assign useful icons for specific locations
-
+var logDelaySec = 1500;
+var placeQueryDistM = 1200;
+var placeRatingThreshold = 3.5;
 // Filter out GMap places types we don't care for, see
 // https://developers.google.com/places/supported_types
 var ignorePlacesTypes = [
-	'food', 'lodging', 'store', 'doctor', 'lawyer'
+	'political', 'food', 'lodging', 'store', 'doctor', 'lawyer', 'atm', 'laundry'
 ];
-var startLocation = { // Beusselstr
-	lat : 52.534350,
-	lng : 13.328834
-};
+var placeNameStripRegex = / GmbH| UG| AG/;
+var queryLocation = {
+	lat: 52.488529642618275, lng: 13.355426788330078
+}
 var cityCntr = { lat: 52.506704, lng: 13.390324 }; // Berlin city center
+
+var map;
+var infowindow;
+var placeMarkers = [];
+var placeRRInterval; // Switches the places round robin
+var placeRRTimeout; // Does the animation back to the map in between places
 
 /** The main app function, invoked by google maps js as soon as its loaded */
 function initMap() {
+	alertify.delay(logDelaySec); // Notification log timeout
 	map = new google.maps.Map(document.getElementById('map'), {
-		center : startLocation,
+		center : queryLocation,
 		zoom : 15
 	});
 	infowindow = new google.maps.InfoWindow(); // For showing popups
-	var queryLocation = new google.maps.LatLng(52.488529642618275, 13.355426788330078);
-
+	var queryLocationLatLng = new google.maps.LatLng(queryLocation.lat, queryLocation.lng);
+	
 	// Search for the farthest carsharing car from the city center
 	queryCarsharing(function cbAvailCars(cars) {
 		var cityCntrLatLng = new google.maps.LatLng(cityCntr.lat, cityCntr.lng);
@@ -48,84 +51,126 @@ function initMap() {
 			map : map,
 			position : carLoc
 		});
+		var bounds = new google.maps.LatLngBounds();
+		bounds.extend(carLoc);
+		bounds.extend(queryLocation);
+		map.fitBounds(bounds); // Re-fit the map on the start-destination bounds
 		google.maps.event.addListener(marker, 'click', function() {
 			infowindow.setContent(chosenCar.value.vehicle.providerName + ': ' + chosenCar.value.vehicle.license);
 			infowindow.open(map, this);
 		});
 		
-		// Search for charging stations near our query location
-		// use the closest charging station to display a route and query
-		// for POIs to show on the map
-		queryAvailChargingStations(queryLocation, function cbChrgStations(stationData) {
-			// Sort the station data by distance from our queryLocation
-			$.each(stationData, function() {
-				var stLatLng = new google.maps.LatLng(this.location.coordinates[1], this.location.coordinates[0]);
-				this.dist = google.maps.geometry.spherical.computeDistanceBetween(queryLocation, stLatLng);
-			})
-			stationData.sort(function(a, b) { // Sort by shortest distance first
-				return a.dist - b.dist;
-			});
-			// Select the nearest station and calculate bounds for the map
-			var dstChrgStn = stationData[0];
-			var dstLatLng = new google.maps.LatLng(dstChrgStn.location.coordinates[1], dstChrgStn.location.coordinates[0]);
-			var bounds = new google.maps.LatLngBounds();
-			bounds.extend(new google.maps.LatLng(startLocation.lat, startLocation.lng));
-			bounds.extend(dstLatLng);
-			map.fitBounds(bounds); // Re-fit the map on the start-destination bounds
-			// Calculate and display the route to the charging stations
-			calcRoute(map, carLoc, dstLatLng);
-			// Query and display interesting places around the charging station
-			function createMarkers(places) {
-				$.each(places, function(idx, place) {
-					var placeLoc = place.geometry.location;
-					var marker = new google.maps.Marker({
-						map : map,
-						position : place.geometry.location
-					});
-					google.maps.event.addListener(marker, 'click', function() {
-						infowindow.setContent(place.name + ' rating: ' + place.rating);
-						infowindow.open(map, this);
-					});
-				});
-			}
+		map.addListener('click', function(ev) {
+			alertify.log('map clicked on ' + ev.latLng.lat().toFixed(3) + ' ' + ev.latLng.lng().toFixed(3));
+			queryAndShowDestinations(map, carLoc, ev.latLng);
+		});
+	});
+}
 
-			function showPlacesRoundRobin(places) {
-				var placeIdx = 0;
-				setInterval(function roundRobinPlaces() {
-					var place = places[placeIdx];
-					var placeLoc = place.geometry.location;
-					var marker = new google.maps.Marker({
-						map : map,
-						position : place.geometry.location
+function clearPlaceMarkers() {
+	$.each(placeMarkers, function() {
+		this.setMap(null);
+	});
+	placeMarkers.length = 0;
+}
+
+/** Query for charging stations and show possible POI destinations */
+function queryAndShowDestinations(map, carLocLatLng, queryLocationLatLng) {
+	if(placeRRInterval) {
+		clearInterval(placeRRInterval);
+		placeRRInterval = null;
+	}
+	if(placeRRTimeout) {
+		clearTimeout(placeRRTimeout);
+		placeRRTimeout = null;
+	}
+		
+	// Search for charging stations near our query location
+	// use the closest charging station to display a route and query
+	// for POIs to show on the map
+	queryAvailChargingStations(queryLocationLatLng, function cbChrgStations(stationData) {
+		// Sort the station data by distance from our queryLocationLatLng
+		$.each(stationData, function() {
+			var stLatLng = new google.maps.LatLng(this.location.coordinates[1], this.location.coordinates[0]);
+			this.dist = google.maps.geometry.spherical.computeDistanceBetween(queryLocationLatLng, stLatLng);
+		})
+		stationData.sort(function(a, b) { // Sort by shortest distance first
+			return a.dist - b.dist;
+		});
+		// Select the nearest station and calculate bounds for the map
+		var dstChrgStn = stationData[0];
+		var dstLatLng = new google.maps.LatLng(dstChrgStn.location.coordinates[1], dstChrgStn.location.coordinates[0]);
+		alertify.log('nearest station is ' + Math.round(dstChrgStn.dist) + 'm away');
+		var bounds = new google.maps.LatLngBounds();
+		bounds.extend(carLocLatLng);
+		bounds.extend(dstLatLng);
+		map.fitBounds(bounds); // Re-fit the map on the start-destination bounds
+		// Calculate and display the route to the charging stations
+		calcRoute(map, carLocLatLng, dstLatLng);
+		// Query and display interesting places around the charging station
+		function createMarkers(places) {
+			clearPlaceMarkers();
+			$.each(places, function(idx, place) {
+				var placeLoc = place.geometry.location;
+				var marker = new google.maps.Marker({
+					map : map,
+					position : place.geometry.location
+				});
+				placeMarkers.push(marker);
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.setContent(place.name + ' rating: ' + place.rating);
+					infowindow.open(map, this);
+				});
+			});
+		}
+
+		function showPlacesRoundRobin(places) {
+			var placeIdx = 0;
+			placeRRInterval = setInterval(function roundRobinPlaces() {
+				clearPlaceMarkers();
+				if(!$('#map').hasClass('pt-page-current'))
+					doPageAnimation($('#poiImage'), $('#map'));
+				var place = places[placeIdx];
+				var placeName = place.name.replace(placeNameStripRegex, ''); // Remove useless suffixes
+				var placeLoc = place.geometry.location;
+				var marker = new google.maps.Marker({
+					map : map,
+					position : placeLoc
+				});
+				placeMarkers.push(marker);
+				map.panTo(placeLoc);
+				infowindow.setContent(placeName + ' ' + place.rating + ' ☆');
+				infowindow.open(map, marker);
+				var photoUrl = '';
+				var photoDim = {'maxWidth': 1280, 'maxHeight': 720};
+				if(place.photos && place.photos.length) {
+					var minPixelWidth = 400;
+					$.each(place.photos, function(idx) {
+						if(this.width >= minPixelWidth) {
+							photoUrl = this.getUrl(photoDim);
+							return false; // break
+						}
 					});
-					var photoUrl = '';
-					var photoDim = {'maxWidth': 1280, 'maxHeight': 720};
-					if(place.photos && place.photos.length) {
-						var minPixelWidth = 400;
-						$.each(place.photos, function(idx) {
-							if(this.width >= minPixelWidth) {
-								photoUrl = this.getUrl(photoDim);
-								return false; // break
-							}
-						});
-					}
+				}
+				var photoImgPreload;
+				if(photoUrl && photoUrl.length > 0) {
+					photoImgPreload = new Image();
+					photoImgPreload.src = photoUrl;
+				}
+				placeRRTimeoutOut = setTimeout(function backToMap() {
 					$('#poiImage').css('background-image', 'url(' + photoUrl + ')');
-					$('#poiTitle').text(place.name.replace(/ GmbH| UG| AG/, '')); // Remove useless suffixes
+					$('#poiTitle').text(placeName);
 					$('#poiIcon').attr('src', place.icon);
 					doPageAnimation($('#map'), $('#poiImage'));
-					
-					setTimeout(function backToMap() {
-						doPageAnimation($('#poiImage'), $('#map'));
-					}, 3000);
-					placeIdx++; // Round robin fashion
-					if(placeIdx >= places.length)
-						placeIdx = 0;
-				}, 4000);
+				}, 3000);
+				placeIdx++; // Round robin fashion
+				if(placeIdx >= places.length)
+					placeIdx = 0;
+			}, 6000);
 
-			}
-//			queryPlaces(map, dstLatLng, createMarkers);
-			queryPlaces(map, dstLatLng, showPlacesRoundRobin);
-		});
+		}
+//		queryPlaces(map, dstLatLng, createMarkers);
+		queryPlaces(map, dstLatLng, showPlacesRoundRobin);
 	});
 }
 
@@ -161,20 +206,17 @@ function queryPlaces(map, latLng, cbBestPlaces) {
 
 	/** Callback for detailed places search, as soon as all results have been fetched */
 	function cbDtlSrch(place, status) {
-		dtlPlaces.push(place);
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-			if(place.rating) { // Only places with ratings
-				var photoDim = {'maxWidth': 400, 'maxHeight': 400};
-				var photoUrl = (place.photos && place.photos.length) ? place.photos[0].getUrl(photoDim) : '';
-				console.log(place, place.name, photoUrl);
-			}
+		if (status !== google.maps.places.PlacesServiceStatus.OK) {
+			console.log('got status ' + status + ' for detail place search');
 		}
+		dtlPlaces.push(place);
 		if(dtlPlaces.length === dtlReqPlaces.length) { // See if we are done
-			dtlPlaces.sort(function(a, b) { // Sort by rating, remove places without one
-				return a.rating - b.rating;
-			}).filter(function() {
-				return this.rating > 3.9 ? true : false;
+			dtlPlaces = dtlPlaces.sort(function(a, b) { // Sort by rating, remove places without one
+				return (!a ? 0 : a.rating) - (!b ? 0 : b.rating);
+			}).filter(function(p) {
+				return (p != null && p.rating > placeRatingThreshold) ? true : false;
 			});
+			alertify.log('found ' + dtlPlaces.length + ' places');
 			cbBestPlaces(dtlPlaces);
 		}
 	}
@@ -193,11 +235,14 @@ function queryPlaces(map, latLng, cbBestPlaces) {
 						break;
 					}
 				}
-				if(ignore)
+				if(ignore) {
+					console.log('Ignoring ' + place.name + ' types: ' + JSON.stringify(place.types));
 					continue;
+				}
 				var dtlReq = {
 					placeId: place.place_id,
 				};
+				console.log('place ' + place.name + ' types: ' + JSON.stringify(place.types));
 				dtlReqPlaces.push(place);
 				service.getDetails(dtlReq, cbDtlSrch);
 			}
@@ -206,10 +251,12 @@ function queryPlaces(map, latLng, cbBestPlaces) {
 	
 	service.nearbySearch({
 		location : latLng,
-		radius : 500,
+		radius : placeQueryDistM,
 		// Only one type is used
 		// type: 'store',
-		type : 'point_of_interest',
+//		type : 'point_of_interest',
+		// Deprecated, but essential, otherwise the results are shit
+		types: ['amusement_park', 'aquarium', 'art_gallery', 'movie_theater', 'museum', 'city_hall', 'shopping_mall', 'stadium', 'university', 'zoo'],
 		rankBy : google.maps.places.RankBy.PROMINENCE,
 	}, cbNbSrch);
 }
@@ -253,7 +300,7 @@ function queryAvailChargingStations(latLng, cbAvailStations) {
 	var allStations = [];
 	var availStations = [];
 	$.get(chrgApiUrl + '/Resources/EVCharging/Stations?lat=' + latLng.lat() + '&lng=' + latLng.lng() + '&radius=4500', function( data ) {
-		console.log(data);
+		console.log('found ' + data.length + ' charging stations ', data);
 		$.each(data, function() {
 			$.get(chrgApiUrl + this.uri, function(stationData) {
 				allStations.push(stationData);
